c7b8aad52e8d493a1dd45c5f6aac4e87ad5845ec
Wiileewaller <33940791+Wiileewaller@users.noreply.github.com>
2019-03-01 02:59:46 +0100

    Add files via upload

---
+++ A020 premiers pas [W].py
@@ -0,0 +1,26 @@
+print("""OPEN CLASSROOM : Apprenez à programmer en Python
+A020 Faites vos premiers pas avec l'interpréteur de commandes Python
+""")
+
+
+print(3.11+2.09)
+# Pourquoi ce dernier résultat approximatif ? Python n'y est pas pour grand chose.\
+# En fait, le problème vient en grande partie de la façon dont les nombres à virgule sont écrits dans la mémoire de votre ordinateur.\
+# C'est pourquoi, en programmation, on préfère travailler autant que possible avec des nombres entiers.\
+# Cependant, vous remarquerez que l'erreur est infime et qu'elle n'aura pas de réel impact sur les calculs.\
+# Les applications qui ont besoin d'une précision mathématique à toute épreuve essayent de pallier ces défauts par d'autres moyens mais ici, ce ne sera pas nécessaire.
+
+
+print(3/4)
+print(3//5)
+print(3/1)
+print(50%13)
+print(13*3)
+
+
+
+print("""--------------------------------------------------
+
+
+for Simplon
+William aka wiileewaller the new coder 17/02""")
